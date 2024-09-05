23c555ae5c8d282227570d5c32da5176677d3801
Reijaff <38776106+Reijaff@users.noreply.github.com>
2024-09-05 07:51:46 +0300

    upgrade

--- __init__.py
+++ __init__.py
@@ -1,18 +1,18 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # ##### END GPL LICENSE BLOCK #####
 
@@ -28,7 +28,10 @@ bl_info = {
     "category": "Sequencer",
 }
 
-import bpy, subprocess, os, sys
+import bpy
+import subprocess
+import os
+import sys
 from bpy.types import Operator
 from bpy.props import (
     IntProperty,
@@ -37,26 +40,22 @@ from bpy.props import (
     StringProperty,
     FloatProperty,
 )
-#import site
-#app_path = site.USER_SITE
-#if app_path not in sys.path:
-#    sys.path.append(app_path)
 
+# Import scenedetect only when needed to avoid potential import errors at startup
 def find_scenes(video_path, threshold, start, end):
-    from scenedetect import open_video#, detect
+    from scenedetect import open_video
     from scenedetect import SceneManager
     from scenedetect.detectors import ContentDetector
-        
+
     render = bpy.context.scene.render
     fps = round((render.fps / render.fps_base), 3)
-    video = open_video(video_path,framerate=fps)
+    video = open_video(video_path, framerate=fps)
     scene_manager = SceneManager()
     scene_manager.add_detector(ContentDetector(threshold=threshold))
-    video.seek((start/fps))
-    scene_manager.detect_scenes(video, end_time=(end/fps))
+    video.seek((start / fps))
+    scene_manager.detect_scenes(video, end_time=(end / fps))
 
     return scene_manager.get_scene_list()
-
 
 class SEQUENCER_OT_split_selected(bpy.types.Operator):
     """Split Unlocked Un/Seleted Strips Soft"""
@@ -76,9 +75,7 @@ class SEQUENCER_OT_split_selected(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        if context.sequences:
-            return True
-        return False
+        return bool(context.sequences)
 
     def execute(self, context):
         selection = context.selected_sequences
@@ -89,31 +86,29 @@ class SEQUENCER_OT_split_selected(bpy.types.Operator):
 
         # find unlocked strips at cursor
         for s in sequences:
-            if s.frame_final_start <= cf and s.frame_final_end > cf:
-                if s.lock == False:
-                    at_cursor.append(s)
-                    if s.select == True:
-                        cut_selected = True
+            if s.frame_final_start <= cf and s.frame_final_end > cf and not s.lock:
+                at_cursor.append(s)
+                cut_selected = cut_selected or s.select
+
         for s in at_cursor:
-            if cut_selected:
-                if s.select:  # only cut selected
-                    bpy.ops.sequencer.select_all(action="DESELECT")
+            if cut_selected and s.select:
+                bpy.ops.sequencer.select_all(action="DESELECT")
+                s.select = True
+                bpy.ops.sequencer.split(
+                    frame=cf,
+                    type=self.type,
+                    side="RIGHT",
+                )
+
+                # add new strip to selection
+                for i in bpy.context.scene.sequence_editor.sequences_all:
+                    if i.select:
+                        selection.append(i)
+                bpy.ops.sequencer.select_all(action="DESELECT")
+                for s in selection:
                     s.select = True
-                    bpy.ops.sequencer.split(
-                        frame=bpy.context.scene.frame_current,
-                        type=self.type,
-                        side="RIGHT",
-                    )
 
-                    # add new strip to selection
-                    for i in bpy.context.scene.sequence_editor.sequences_all:
-                        if i.select:
-                            selection.append(i)
-                    bpy.ops.sequencer.select_all(action="DESELECT")
-                    for s in selection:
-                        s.select = True
         return {"FINISHED"}
-
 
 class SEQUENCER_OT_detect_shots(Operator):
     """Detect shots in active strip and split all selected strips accordingly"""
@@ -124,51 +119,43 @@ class SEQUENCER_OT_detect_shots(Operator):
 
     @classmethod
     def poll(cls, context):
-        if (
+        return (
             context.scene
             and context.scene.sequence_editor
             and context.scene.sequence_editor.active_strip
-        ):
-            return context.scene.sequence_editor.active_strip.type == "MOVIE"
-        else:
-            return False
+            and context.scene.sequence_editor.active_strip.type == "MOVIE"
+        )
 
     def execute(self, context):
         scene = context.scene
         sequencer = bpy.ops.sequencer
         cf = context.scene.frame_current
         path = context.scene.sequence_editor.active_strip.filepath
-        path = (os.path.realpath(bpy.path.abspath(path))).replace("\\", "\\\\")
+        path = os.path.realpath(bpy.path.abspath(path))
 
-        msg = "Please wait. Detecting shots in "+str(path)+"."
-        self.report({'INFO'}, msg)
-        
-        path = path.replace("\\", "\\\\")
+        self.report({'INFO'}, f"Please wait. Detecting shots in {path}.")
+
         active = context.scene.sequence_editor.active_strip
         start_time = active.frame_offset_start
         end_time = active.frame_duration - active.frame_offset_end
         scenes = find_scenes(path, 32, start_time, end_time)
-        for i, scene in enumerate(scenes):
-            context.scene.frame_current = int(scene[1].get_frames()+active.frame_start)
+        for scene in scenes:
+            context.scene.frame_current = int(scene[1].get_frames() + active.frame_start)
             sequencer.split_selected()
 
         context.scene.frame_current = cf
 
-        msg = "Finished: Shot detection and strip splitting."
-        self.report({'INFO'}, msg)
+        self.report({'INFO'}, "Finished: Shot detection and strip splitting.")
         return {'FINISHED'}
-
 
 def menu_detect_shots(self, context):
     self.layout.separator()
     self.layout.operator("sequencer.detect_shots")
 
-
 classes = (
     SEQUENCER_OT_detect_shots,
     SEQUENCER_OT_split_selected,
 )
-
 
 def register():
     for cls in classes:
@@ -176,13 +163,11 @@ def register():
     bpy.types.SEQUENCER_MT_context_menu.append(menu_detect_shots)
     bpy.types.SEQUENCER_MT_strip.append(menu_detect_shots)
 
-
 def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
     bpy.types.SEQUENCER_MT_context_menu.remove(menu_detect_shots)
     bpy.types.SEQUENCER_MT_strip.remove(menu_detect_shots)
-
 
 if __name__ == "__main__":
     register()
